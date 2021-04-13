f95e8c38a8125d444228090a59019b996661d52a
umsyt <>
2021-04-13 22:31:37 +0000

    almost stoned

--- main.py
+++ main.py
@@ -25,13 +25,23 @@ desc = {"cr":"A dark, seemingly dingy room. You can make out a screen.",
         "dar":"Nothing but a scrap of [paper].",
         "dkr":"Another [keypad]? This one has a full keyboard.",
         "war":"nothing yet. war",
-        "car":"nothing yet. car",
-        "wkr":"nothing yet. wkr",
-        "ckr":"nothing yet. ckr"}
+        "car":"Mostly empty. Just a [blue-rock].",
+        "wkr":"Mostly empty. Just a [red-rock].",
+        "ckr":"Mostly empty. Just a [green-rock]."}
 abrv = {"d": "[d]eosil",
         "w": "[w]iddershins",
         "a": "[a]na",
         "k": "[k]ata"}
+objs = {
+  "red-rock":"wkr",
+  "green-rock":"ckr",
+  "blue-rock":"car"
+}
+slots = {
+  "1" : ""
+  "2" : ""
+  "3" : ""
+}
 
 print("""
 BRAINDOWN: THE INTERACTIVE FRICTION
@@ -47,12 +57,18 @@ if name == "":
   name = "Unnamed Traveler"
 
 print(f"""
-It seems you're on your own {name}.""")
+Amazing.
+Actions and objects are written in [brackets].
+You can check your location with "whereami" and inventory with "inv".
+You're on your own from here, {name}.
+...
+""")
 
 tvtsr = False
 
 while not solved and not tvtsr:
   #describe
+  
   print(desc[me['loc']])
   
   if me['loc'] == "cr":
@@ -67,6 +83,7 @@ while not solved and not tvtsr:
    else:
      print("The switch is off.")
 
+  print()
 
   print("current exits:")
   for i in exits[me['loc']].keys():
@@ -84,6 +101,14 @@ while not solved and not tvtsr:
 
   if act[0] in "dawk":
     me['loc'] = exits[me['loc']][act[0]]
+  
+  if act[0] in ["i", "inv", "inventory"]:
+    for obj in objs.keys():
+      if objs[obj] == "inv":
+        print("-"+obj+"-")
+
+  if act[0] in ["location", "where", "whereami"]:
+    print(me['loc'])
 
   if rp("toggle","wr"):
     switc = not switc
@@ -92,7 +117,7 @@ while not solved and not tvtsr:
     print("It awaits a [code <set of digits>].")
   
   if rp("code","dr"):
-    if act[1] == str(code):
+    if act[1] == str(code) or act[1] == "inri":
       print("The device whirrs...")
       exits["dr"]["a"] = "dar"
       exits["dr"]["k"] = "dkr"
@@ -117,7 +142,7 @@ You can now move in directions [a]na and [k]ata in TOS enabled rooms            
     """)
 
   if rp("keypad","dkr"):
-    print("give me da [key <string>]!!! >.< ")
+    print("give me da [key <string>]!!! 🥺 ")
   
   if rp("key","dkr"):
     if act[1] == (woah[::-1] + chr(121)):
@@ -126,6 +151,24 @@ You can now move in directions [a]na and [k]ata in TOS enabled rooms            
       exits["dar"]["d"] = "war"
       exits["dkr"]["w"] = "ckr"
       exits["dkr"]["d"] = "wkr"
+    else:
+      print("bad key...")
+  nune = "Mostly empty."
+
+  if rp("red-rock", objs["red-rock"]) :
+    print("You now have the red rock.")
+    objs["red-rock"] = "inv"
+    desc[me["loc"]] = nune
+
+  if rp("green-rock", objs["green-rock"]):
+    print("You now have the green rock.")
+    objs["green-rock"] = "inv"
+    desc[me["loc"]] = nune
+
+  if rp("blue-rock", objs["blue-rock"]):
+    print("You now have the blue rock.")
+    objs["blue-rock"] = "inv"
+    desc[me["loc"]] = nune
 
   if act[0] == "letmeoutp":
     solved = True
